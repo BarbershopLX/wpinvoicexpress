f4872f5b352ffa72bcdb3cb76fca83ea8b8c41b5
Rui Cruz <mail@ruicruz.com>
2015-07-28 15:54:05 +0100

    update plugin version

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPInvoicexpress
  * Plugin URI: http://barbershop.pt/wp-invoicexpress-plugin/
  * Description: Add finantial information to your Wordpress Dashboard from Invoicexpress.
- * Version: 1.0
+ * Version: 1.0.1
  * Author: Rui Cruz @ barbershop.pt
  * Author URI: http://barbershop.pt
  */
